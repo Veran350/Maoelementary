51590e81304013903fc27079d7f4bc760b292ac9
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-23 00:53:18 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-// src/App.js
 import React from 'react';
 import ProductList from './components/ProductList';  // Import the ProductList component
+import CreateProduct from './components/CreateProduct';  // Import the CreateProduct component
 import './App.css';  // If you have any styling for your app
 
 const App = () => {
@@ -10,7 +10,9 @@ const App = () => {
         <h1>Maoelementary Store</h1>
       </header>
       <main>
-        <ProductList />  {/* Render ProductList component */}
+        <h2>Admin Panel</h2>
+        <CreateProduct />  {/* Render CreateProduct component for adding new products */}
+        <ProductList />  {/* Render ProductList component to display the list of products */}
       </main>
     </div>
   );
