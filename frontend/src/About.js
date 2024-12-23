7bcc89f19b90cf952dc5e02295a261330634710f
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-23 18:47:27 +0100

    Create About.js

---
+++ frontend/src/About.js
@@ -0,0 +1,13 @@
+// src/About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Maoelementary</h1>
+      <p>This is a Shopify-like e-commerce platform built with React and Node.js.</p>
+    </div>
+  );
+}
+
+export default About;
