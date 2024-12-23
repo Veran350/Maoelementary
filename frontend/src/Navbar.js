148e85cab988c914b52eda05f0668b720edc6903
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-23 19:33:05 +0100

    Update Navbar.js

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,15 +1,14 @@
-// src/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/products">Products</Link></li>
+    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
+      <ul style={{ listStyleType: 'none', display: 'flex', justifyContent: 'space-around' }}>
+        <li><Link style={{ color: '#fff', textDecoration: 'none' }} to="/">Home</Link></li>
+        <li><Link style={{ color: '#fff', textDecoration: 'none' }} to="/dashboard">Dashboard</Link></li>
+        <li><Link style={{ color: '#fff', textDecoration: 'none' }} to="/about">About</Link></li>
+        <li><Link style={{ color: '#fff', textDecoration: 'none' }} to="/products">Products</Link></li>
       </ul>
     </nav>
   );
