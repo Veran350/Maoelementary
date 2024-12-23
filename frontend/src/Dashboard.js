fb99a1420ce74331b5f82f2de6b2dce0768c98f8
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-23 18:46:25 +0100

    Create Dashboard.js

---
+++ frontend/src/Dashboard.js
@@ -0,0 +1,13 @@
+// src/Dashboard.js
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Here you can manage your products and orders.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
