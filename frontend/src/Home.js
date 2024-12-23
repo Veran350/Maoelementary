d8f99fd7f142fb2a8f5296587788da378e86bc0d
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-23 18:46:06 +0100

    Create Home.js

---
+++ frontend/src/Home.js
@@ -0,0 +1,14 @@
+// src/Home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Maoelementary</h1>
+      <p>Your maoelementary-like e-commerce solution</p>
+    </div>
+  );
+}
+
+export default Home;
+￼Enter
