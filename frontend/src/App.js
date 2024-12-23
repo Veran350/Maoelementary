69bd0d8ad44d1ff90ea80295f1ee7da49648cebf
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-23 19:12:41 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<ProductPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
       </Routes>
     </Router>
   );
