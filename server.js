310c52ee9489b112904704978d11daef2070c1f8
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-23 13:20:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors());
 
 // MongoDB connection
 const MONGO_URI = process.env.MONGO_URI; // Use the MongoDB URI from .env
+console.log('MongoDB URI:', MONGO_URI); // Debugging: Log the MongoDB URI
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('MongoDB connection error:', err));
