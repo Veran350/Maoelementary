9910779d808e7db8f3fa09f9b9cff1354fffe81a
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-23 18:48:21 +0100

    Create ProductPage.js

---
+++ frontend/src/ProductPage.js
@@ -0,0 +1,13 @@
+// src/ProductPage.js
+import React from 'react';
+
+function ProductPage() {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>Here are some amazing products that we offer!</p>
+    </div>
+  );
+}
+
+export default ProductPage;
