7d75fa73c4c98312e98189d1140a773c044b4d0a
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-23 20:17:02 +0100

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  image: { type: String, required: true },
-  category: { type: String, required: true },
-  stock: { type: Number, required: true },
-  timestamp: { type: Date, default: Date.now },
-});
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    default: true,
+  },
+}, { timestamps: true });
 
-const Product = mongoose.model('Product', productSchema);
-
-module.exports = Product;
+module.exports = mongoose.model('Product', productSchema);
