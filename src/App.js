13baf564ca0a15a4349ebd54271ce84c4b0df4f5
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-27 10:32:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to Maoelementary</h1>
-    </div>
+    <Router>
+      <div>
+        <h1>Welcome to Maoelementary</h1>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
