7a1592d271b1e5e2e64538ea0b182d1c11dd53fe
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-27 11:19:51 +0100

    Create Cartpage.js

---
+++ src/pages/Cartpage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useCart } from '../context/CartContext';
+
+const CartPage = () => {
+  const { cart, removeFromCart } = useCart();
+
+  return (
+    <div>
+      <h1>Your Cart</h1>
+      {cart.length > 0 ? (
+        cart.map((item) => (
+          <div key={item._id}>
+            <h3>{item.name}</h3>
+            <p>Price: ${item.price}</p>
+            <button onClick={() => removeFromCart(item._id)}>Remove</button>
+          </div>
+        ))
+      ) : (
+        <p>Your cart is empty.</p>
+      )}
+    </div>
+  );
+};
+
+export default CartPage;
