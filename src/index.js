fe5b553be9690255fea46b2eb7646b22d5112f28
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-27 11:29:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { CartProvider } from './context/CartContext'; // Import CartProvider
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <CartProvider>  {/* Wrap the App with CartProvider */}
+    <App />
+  </CartProvider>,
+  document.getElementById('root')
+);
