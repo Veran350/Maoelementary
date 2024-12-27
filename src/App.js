87810a17c422ae977dfb27fda2a532d1c1e252cc
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-27 11:17:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import HomePage from './pages/HomePage';
+import ProductDetails from './pages/ProductDetails';  // Import the ProductDetails page
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/products/:id" element={<ProductDetails />} />  {/* New route for product details */}
         </Routes>
       </div>
     </Router>
