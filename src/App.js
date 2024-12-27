578fe3fac20a10829e77e5bd5c2f5d5bcea1ef95
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-27 11:31:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import HomePage from './pages/HomePage';
-import ProductDetails from './pages/ProductDetails';  // Import the ProductDetails page
+import ProductDetails from './pages/ProductDetails';
+import CartPage from './pages/CartPage';  // Import CartPage
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/products/:id" element={<ProductDetails />} />  {/* New route for product details */}
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<CartPage />} /> {/* Add the CartPage route */}
         </Routes>
       </div>
     </Router>
